440c1845a3a61efdae73820cd2415ce2405f2d38
Yokan-Hagane <gabi.nerd05@gmail.com>
2026-06-30 21:49:17 -0300

    Implementação de display.py e tabela()

---
+++ display.py
@@ -0,0 +1,27 @@
+def tabela(cur):
+    if cur.description is None:
+        printf(f"{cur.rowcount} linhas(s) afetada(s).")
+        return
+
+    colunas = [d[0] for d in cur.description]
+    linhas = cur.fetchall()
+
+    if not linhas:
+        print("nenhum resultado")
+        return
+
+    larguras = [len(c) for c in colunas]
+    for l in linhas:
+        for i,v in enumerate(l):
+            larguras[i] = max(larguras[i], len(str(v)))
+
+    sep = "+-" + "-+-".join("-" * l for l in larguras) + "-+"
+    fmt = "| " + " | ".join(f"{{:<{l}}}" for l in larguras) + " |"
+
+    print(sep)
+    print(fmt.format(*colunas))
+    print(sep)
+    for linha in linhas:
+        print(fmt.format(*[str(v) for v in linha]))
+    print(sep)
+    print(f"({len(linhas)} linha(s))\n")
